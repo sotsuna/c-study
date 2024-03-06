c5fcb7152a0b46488f85276b4e7e12d47616e258
sotsuna <mkist202@gmail.com>
2024-03-06 16:03:41 -0300

    finished perimetro_retangulo

--- formulas/perimetro_triangulo.c
+++ formulas/perimetro_triangulo.c
@@ -8,4 +8,20 @@ int main(){
 
     setlocale(LC_ALL, "Portuguese_Brazil.65001");
 
-    // p = lado1 + lado2 + lado2
+    double lado1, lado2, lado3, P;
+
+    printf("Digite o valor do lado 1: ");
+    scanf("%lf", &lado1);
+
+    printf("Digite o valor do lado 2: ");
+    scanf("%lf", &lado2);
+
+    printf("Digite o valor do lado 3: ");
+    scanf("%lf", &lado3);
+
+    P = lado1 + lado2 + lado3;
+
+    printf("O valor de perímetro é: %.1lf\n", P);
+
+    return 0;
+}
